4b4a015b0582cddd000049358cbf42f12931141b
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-08 16:12:42 -0500

    added href paths & filled product details pages

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,47 +1,8 @@
 const fs = require('fs')
-// fs is file system; access to reading and writing data right to the file system; return an object with lots of functions to use stored in fs variable
 const http = require('http')
 // set-up to create web server capable of accepting requests and sending back responses; http gives networking capabilities
 const url = require('url')
 // able to analyze for routing
-
-// const hello = 'Hello World'
-// console.log(hello)
-// in terminal, "node Index.js" will run the Index.js file in the terminal
-
-
-// BLOCKING SYNCRONOUS WAY
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
-// console.log(textIn)
-// Example of how to read a file
-// sync for synchronous
-// first param if the file path, second is defining the character encoding
-// calling this function will read the data in the file and return it
-
-// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`
-// what we want to write to the file
-// fs.writeFileSync('./starter/txt/output.txt', textOut)
-// console.log('File written')
-// Example of how to write to a file
-// first param is the file path, second is what we want to write
-
-
-// NON-BLOCKING ASYNCHRONOUS WAY
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//     if (err) return console.log('Error!')
-//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         if (err) return console.log('Error!')
-//         console.log(data2)
-//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-//             if (err) return console.log('Error!')
-//             console.log(data3)
-//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//                 if (err) return console.log('Error!')
-//                 console.log('File has been written')
-//             })
-//         })
-//     })    
-// })
 
 
 // FILES
@@ -74,10 +35,10 @@ const dataObj = JSON.parse(data) /*JSON.parse will take the JSON string and turn
 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url
+    const {query, pathname} = url.parse(req.url, true)
 
     // Overview page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead (200, {'Content-type': 'text/html'})
 
         const cardsHtml = dataObj.map(elem => replaceTemplate(tempCard, elem)).join('')
@@ -86,11 +47,14 @@ const server = http.createServer((req, res) => {
         res.end(output)
 
     // Product page
-    } else if (pathName === '/product') {
-        res.end('This is the Product')
+    } else if (pathname === '/product') {
+        res.writeHead (200, {'Content-type': 'text/html'})
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
 
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
             res.writeHead (200, {'Content-type': 'application/json'})
             res.end(data)
 
