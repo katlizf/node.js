dddb71c64bc57b1644fee57c5b03f457303650ac
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-08 15:22:29 -0500

    html to template & replace temp info w/prod info

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -49,19 +49,52 @@ const url = require('url')
 
 
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    // /.../g replaces all global instances of ...
+    output = output.replace (/{%IMAGE%}/g, product.image)
+    output = output.replace (/{%PRICE%}/g, product.price)
+    output = output.replace (/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace (/{%QUANTITY%}/g, product.quantity)
+    output = output.replace (/{%DESCRIPTION%}/g, product.description)
+    output = output.replace (/{%ID%}/g, product.id)
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+    return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
+
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
-const projectData = JSON.parse(data) /*JSON.parse will take the JSON string and turn it into JS*/
+const dataObj = JSON.parse(data) /*JSON.parse will take the JSON string and turn it into JS*/
 // top-level code that is only executed once place at the beginning, is blocking code
+
 
 const server = http.createServer((req, res) => {
     const pathName = req.url
+
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the Overview')
+        res.writeHead (200, {'Content-type': 'text/html'})
+
+        const cardsHtml = dataObj.map(elem => replaceTemplate(tempCard, elem)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+        res.end(output)
+
+    // Product page
     } else if (pathName === '/product') {
         res.end('This is the Product')
+
+    // API
     } else if (pathName === '/api') {
             res.writeHead (200, {'Content-type': 'application/json'})
             res.end(data)
+
+    // Not found
         } else {
             res.writeHead(404, {
                 'Contect-type': 'text/html',
@@ -71,7 +104,7 @@ const server = http.createServer((req, res) => {
     }
     // setting up routing
 
-    res.end('Hello from the server') /*way to send response back from the server*/
+    // res.end('Hello from the server') /*way to send response back from the server*/
 })
 // this function will run everytime a request is made
 
