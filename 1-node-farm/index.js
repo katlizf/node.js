4f4bb2647f0576f57fec0bd3f9317b092552be55
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-08 13:06:45 -0500

    practice with fs.readFile async way

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 // console.log(hello)
 // in terminal, "node Index.js" will run the Index.js file in the terminal
 
+
+// BLOCKING SYNCRONOUS WAY
 const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
 console.log(textIn)
 // Example of how to read a file
@@ -18,3 +20,21 @@ fs.writeFileSync('./starter/txt/output.txt', textOut)
 console.log('File written')
 // Example of how to write to a file
 // first param is the file path, second is what we want to write
+
+
+// NON-BLOCKING ASYNCHRONOUS WAY
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error!')
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        if (err) return console.log('Error!')
+        console.log(data2)
+        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+            if (err) return console.log('Error!')
+            console.log(data3)
+            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                if (err) return console.log('Error!')
+                console.log('File has been written')
+            })
+        })
+    })    
+})
