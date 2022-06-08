751527ea1f6b34e9ce6fb2ab5a47d7c05e9a8c2c
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-08 14:20:11 -0500

    server & routing setup

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 // fs is file system; access to reading and writing data right to the file system; return an object with lots of functions to use stored in fs variable
+const http = require('http')
+// set-up to create web server capable of accepting requests and sending back responses; http gives networking capabilities
+const url = require('url')
+// able to analyze for routing
 
 // const hello = 'Hello World'
 // console.log(hello)
@@ -7,34 +11,72 @@ const fs = require('fs')
 
 
 // BLOCKING SYNCRONOUS WAY
-const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
-console.log(textIn)
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
+// console.log(textIn)
 // Example of how to read a file
 // sync for synchronous
 // first param if the file path, second is defining the character encoding
 // calling this function will read the data in the file and return it
 
-const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`
+// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`
 // what we want to write to the file
-fs.writeFileSync('./starter/txt/output.txt', textOut)
-console.log('File written')
+// fs.writeFileSync('./starter/txt/output.txt', textOut)
+// console.log('File written')
 // Example of how to write to a file
 // first param is the file path, second is what we want to write
 
 
 // NON-BLOCKING ASYNCHRONOUS WAY
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('Error!')
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        if (err) return console.log('Error!')
-        console.log(data2)
-        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-            if (err) return console.log('Error!')
-            console.log(data3)
-            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                if (err) return console.log('Error!')
-                console.log('File has been written')
-            })
-        })
-    })    
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('Error!')
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         if (err) return console.log('Error!')
+//         console.log(data2)
+//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//             if (err) return console.log('Error!')
+//             console.log(data3)
+//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 if (err) return console.log('Error!')
+//                 console.log('File has been written')
+//             })
+//         })
+//     })    
+// })
+
+
+// FILES
+
+
+
+// SERVER
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+const projectData = JSON.parse(data) /*JSON.parse will take the JSON string and turn it into JS*/
+// top-level code that is only executed once place at the beginning, is blocking code
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the Overview')
+    } else if (pathName === '/product') {
+        res.end('This is the Product')
+    } else if (pathName === '/api') {
+            res.writeHead (200, {'Content-type': 'application/json'})
+            res.end(data)
+        } else {
+            res.writeHead(404, {
+                'Contect-type': 'text/html',
+                'My-own-header': 'hello-world' /*header written as object, can use to send some metadata about the response, can write your own header*/
+            }) /*writes status code in dev tools console*/
+        res.end('Page not found')
+    }
+    // setting up routing
+
+    res.end('Hello from the server') /*way to send response back from the server*/
 })
+// this function will run everytime a request is made
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
+// first param is port number, second is local host(typically 127.0.0.1 as default)
+// server.listen starts up server to start listening for requests
