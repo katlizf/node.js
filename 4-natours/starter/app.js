f7fbc11d48a6c0885da455e178ebfb61471b06d0
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-09 14:34:34 -0500

    created better file structure

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,17 @@
-const { resolveObjectURL } = require('buffer')
 const express = require('express')
-const fs = require('fs')
-const PORT  = 3000
+const morgan = require('morgan')
 const app = express()
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-// Middleware that applies to every request
+// MIDDLEWARE, applies to every request
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+// can show details such as "GET /api/v1/tours 200 11.544 ms - 8618" in terminal
+
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
     console.log('Hello from the middleware')
     next()
@@ -16,100 +22,8 @@ app.use((req, res, next) => {
     next()
 })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+// ROUTES, mounted
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-    })
-}
-
-const getTour = (req, res) => {
-    console.log(req.params)
-    const id = req.params.id * 1
-    const tour = tours.find(elem => elem.id === id)
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tour
-        }
-    })
-}
-
-const createTour = (req, res) => {
-    const newID = tours[tours.length-1].id +1
-    const newTour = Object.assign({id: newID}, req.body)
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-}
-
-const updateTour = (req, res) => {
-    if (req.params.id*1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
-}
-
-const deleteTour = (req, res) => {
-    if (req.params.id*1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-}
-
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id', getTour)
-// app.post('/api/v1/tours', createTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
-
-// get mtheod is the http for the request
-// can add optional params to url with "/:param?"
-
-// Refactored way of coding the above routes
-// Middleware that responds to specific requests
-app.route('/api/v1/tours')
-    .get(getAllTours)
-        .post(createTour)
-app.route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour)
-
-
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`)
-})
+module.exports = app
