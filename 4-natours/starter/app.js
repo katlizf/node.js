f7817061780b15686c16cc284c5282abc667d758
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-09 15:28:30 -0500

    environment variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,22 +4,22 @@ const app = express()
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
-// MIDDLEWARE (applies to every request)
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-}
-// can show details such as "GET /api/v1/tours 200 11.544 ms - 8618" in terminal
 app.use(express.json())
+
+// MIDDLEWARE (applies to every request)
 app.use(express.static(`${__dirname}/public`))
 app.use((req, res, next) => {
     console.log('Hello from the middleware')
     next()
 })
-// next calls the next middleware in the stack to run
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
 })
+// next calls the next middleware in the stack to run
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')) /*can show details such as "GET /api/v1/tours 200 11.544 ms - 8618" in terminal*/
+}
 
 
 // ROUTES, mounted
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
 const app = require('./app')
-const PORT  = 3000
+
+// console.log(app.get('env'))
+// console.log(process.env)
+// see the environment variable
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
