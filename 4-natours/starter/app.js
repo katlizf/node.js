268e6fe573d01c8ab311d395a7e44b4c21a80432
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-09 13:22:25 -0500

    setting up middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,14 +4,24 @@ const fs = require('fs')
 const PORT  = 3000
 const app = express()
 
+// Middleware that applies to every request
 app.use(express.json())
-
+app.use((req, res, next) => {
+    console.log('Hello from the middleware')
+    next()
+})
+// next calls the next middleware in the stack to run
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    next()
+})
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
+        requestedAt: req.requestTime,
         results: tours.length,
         data: {
             tours: tours
@@ -90,6 +100,7 @@ const deleteTour = (req, res) => {
 // can add optional params to url with "/:param?"
 
 // Refactored way of coding the above routes
+// Middleware that responds to specific requests
 app.route('/api/v1/tours')
     .get(getAllTours)
         .post(createTour)
